9c79338fc94f76393cdabd4db6a54edf318f43e1
William Gram <william.gram@pwc.com>
2022-10-07 09:15:44 +0200

    class development

--- c_stock_data.py
+++ c_stock_data.py
@@ -1,14 +1,32 @@
-class StockData:
-    def __init__(self, yfin_data):
+class YFinData:
+    def __init__(self, yfin_data=None, tickers=None, start_date=None, end_date=None):
         self.yfin_data = yfin_data
+        self.tickers = tickers
+        self.start_date = start_date
+        self.end_date = end_date
         self.stock_df = None
         self.returns_df = None
     
-    def mk_df(self):
-        stock_df = self.yfin_data.stack().reset_index().rename(columns={'level_1': 'Ticker'})
-        stock_df.columns = stock_df.columns.str.replace(' ', '_')
+    def make_df(self):
+        stock_df = self.yfin_data \
+            .stack() \
+            .reset_index() \
+            .rename(columns={'level_1': 'Ticker'})
+        
+        stock_df.columns = stock_df \
+            .columns \
+            .str \
+            .replace(' ', '_')
+        
         self.stock_df = stock_df
         print('Attribute "stock_df" was assigned to this class object, see self.stock_df.')
+    
+    def get_df(self):
+        if self.stock_df is not None:
+            return self.stock_df
+        else:
+            self.make_df(self)
+            return self.stock_df
     
     def get_returns(self):
         if self.stokc_df is None:
@@ -21,5 +39,8 @@ class StockData:
             .set_index(['Ticker', 'Date']) \
             .diff() \
             .reset_index() \
-            [cols]
-        print('Attribute "returns_df" was assigned to this class instance, see self.returns_df.')
+            .loc[:, cols]
+        print(
+            'Attribute "returns_df" was assigned to this class instance, '\
+            ' see self.returns_df.'
+        )
